4835f9bf82a3dd865efe85482015fc5b022b52ce
likebale <108213900+likebale@users.noreply.github.com>
2022-08-13 19:33:36 +0900

    Update add_service.sh

--- etc/systemd/system/add_service.sh
+++ etc/systemd/system/add_service.sh
@@ -1,12 +1,14 @@
 
 
 sudo ln -s /usr/local/zookeeper/conf/zookeeper.service zookeeper.service
-sudo systemctl status zookeeper.service
 sudo systemctl daemon-reload
 sudo systemctl enable zookeeper.service
+sudo systemctl start zookeeper.service
+sudo systemctl status zookeeper.service
 
 
 sudo ln -s /usr/local/kafka/config/kafka.service kafka.service
-sudo systemctl status kafka.service
 sudo systemctl daemon-reload
 sudo systemctl enable kafka.service
+sudo systemctl start kafka.service
+sudo systemctl status kafka.service
